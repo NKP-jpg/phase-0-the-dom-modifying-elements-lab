da031265d60263d692c88073deb050e1ab0330dd
Nija <nija.perry21@gmail.com>
2021-03-28 21:29:29 -0400

    commit tree 998877f95147c4f3bd42d7869c5dfb7d24a06e69

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+let name = "Nija"
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = `${name}` + " is the champion";
+document.body.appendChild(newHeader);
